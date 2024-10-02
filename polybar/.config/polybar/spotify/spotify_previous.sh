47505d5014620ee26a139161d1ce514496647e47
chris <chris.schoony@gmail.com>
2024-10-01 17:54:56 -0700

    fix spotify

--- polybar/.config/polybar/spotify/spotify_previous.sh
+++ polybar/.config/polybar/spotify/spotify_previous.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STATUS=$(playerctl --player=spotify status 2>/dev/null)
+SONG_INFO=$(playerctl --player=spotify metadata --format "{{ artist}} - {{ title }}" 2>/dev/null)
 
 if [ $? -eq 0 ]; then
   echo "󰒮"
