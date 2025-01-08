7b2c94b22938eb0d9e9ab132327c6572db97685d
chris <chris.schoony@gmail.com>
2025-01-07 16:26:38 -0800

    fix alt

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,6 +7,7 @@ config.color_scheme = "catppuccin-mocha"
 config.font = wezterm.font("Mononoki Nerd Font Mono")
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
+config.send_composed_key_when_right_alt_is_pressed = false
 
 font_size.set(config)
 
