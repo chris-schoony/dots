1b112dc32d5b9cb00becfbb452833d07de276f74
chris <chris.schoony@gmail.com>
2025-05-26 22:41:54 -0700

    fix lspconfig?

--- nvim/.config/nvim/lua/plugin_configs/lsp/init.lua
+++ nvim/.config/nvim/lua/plugin_configs/lsp/init.lua
@@ -1,95 +1,94 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    {
-      "williamboman/mason.nvim",
-      dependencies = { 'mason-org/mason-registry' },
-      config = true,
-    },
-    "williamboman/mason-lspconfig.nvim",
-    "saghen/blink.cmp",
-    {
-      "j-hui/fidget.nvim",
-      opts = {},
-    },
-    {
-      "b0o/schemastore.nvim",
-    },
-    {
-      "nvimdev/lspsaga.nvim",
-      opts = {
-        symbol_in_winbar = {
-          enable = false,
-        },
-        lightbulb = {
-          enable = false,
-        },
-      },
-    },
-  },
-  config = function()
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-      callback = function(event)
-        local map = function(keys, func, desc)
-          if desc then
-            desc = "LSP: " .. desc
-          end
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{
+			"williamboman/mason.nvim",
+			dependencies = { "mason-org/mason-registry" },
+			config = true,
+		},
+		"williamboman/mason-lspconfig.nvim",
+		"saghen/blink.cmp",
+		{
+			"j-hui/fidget.nvim",
+			opts = {},
+		},
+		{
+			"b0o/schemastore.nvim",
+		},
+		{
+			"nvimdev/lspsaga.nvim",
+			opts = {
+				symbol_in_winbar = {
+					enable = false,
+				},
+				lightbulb = {
+					enable = false,
+				},
+			},
+		},
+	},
+	config = function()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+			callback = function(event)
+				local map = function(keys, func, desc)
+					if desc then
+						desc = "LSP: " .. desc
+					end
 
-          vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc })
-        end
+					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc })
+				end
 
-        map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-        map("<leader>ca", ":Lspsaga code_action<CR>", "[C]ode [A]ction")
-        map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-        map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-        map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-        map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-        map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-        map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-        map("K", vim.lsp.buf.hover, "Hover Documentation")
-        map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-        map("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-        map("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-        map("<leader>wl", function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, "[W]orkspace [L]ist Folders")
-      end,
-    })
+				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+				map("<leader>ca", ":Lspsaga code_action<CR>", "[C]ode [A]ction")
+				map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+				map("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+				map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+				map("K", vim.lsp.buf.hover, "Hover Documentation")
+				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+				map("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+				map("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+				map("<leader>wl", function()
+					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				end, "[W]orkspace [L]ist Folders")
+			end,
+		})
 
-    local servers = require("plugin_configs.lsp.servers")
+		local servers = require("plugin_configs.lsp.servers")
 
-    require("mason").setup()
-    local mason_lspconfig = require("mason-lspconfig")
+		require("mason").setup()
+		local mason_lspconfig = require("mason-lspconfig")
+		local lspconfig = require("lspconfig")
 
-    mason_lspconfig.setup({
-      ensure_installed = vim.tbl_keys(servers),
-    })
+		mason_lspconfig.setup({
+			ensure_installed = vim.tbl_keys(servers),
+			handlers = {
+				function(server_name)
+					local server = servers[server_name] or {}
+					server.capabilities = require("blink.cmp").get_lsp_capabilities(server.capabilities)
+					lspconfig[server_name].setup(server)
+				end,
+			},
+		})
 
-    local lspconfig = require("lspconfig")
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        local server = servers[server_name]
-        server.capabilities = require("blink.cmp").get_lsp_capabilities(server.capabilities)
-        -- server.capabilities.textDocument.completion.completionItem.snippetSupport = true
-        lspconfig[server_name].setup(server)
-      end,
-    })
-    vim.diagnostic.config({
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = "󰅚 ",
-          [vim.diagnostic.severity.WARN] = "󰀪 ",
-          [vim.diagnostic.severity.HINT] = "󰌶 ",
-          [vim.diagnostic.severity.INFO] = "  ",
-        },
-        numhl = {
-          [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-          [vim.diagnostic.severity.WARN] = "WarningMsg",
-          [vim.diagnostic.severity.HINT] = "HintMsg",
-          [vim.diagnostic.severity.INFO] = "InfoMsg",
-        },
-      },
-    })
-  end,
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "󰅚 ",
+					[vim.diagnostic.severity.WARN] = "󰀪 ",
+					[vim.diagnostic.severity.HINT] = "󰌶 ",
+					[vim.diagnostic.severity.INFO] = "  ",
+				},
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+					[vim.diagnostic.severity.WARN] = "WarningMsg",
+					[vim.diagnostic.severity.HINT] = "HintMsg",
+					[vim.diagnostic.severity.INFO] = "InfoMsg",
+				},
+			},
+		})
+	end,
 }
