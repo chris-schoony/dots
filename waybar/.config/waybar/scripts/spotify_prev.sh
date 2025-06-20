7467d607bcf5acab624419647993aeef5ae287da
chris <chris.schoony@gmail.com>
2025-06-20 14:57:18 -0700

    fix spotify scripts and update nvim plugins

--- waybar/.config/waybar/scripts/spotify_prev.sh
+++ waybar/.config/waybar/scripts/spotify_prev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SONG_INFO=$(playerctl --player=spotify metadata --format "{{ artist}} - {{ title }}" 2>/dev/null)
+STATUS=$(playerctl --player=spotify status 2>/dev/null)
 
 if [ $? -eq 0 ]; then
   echo "󰒮"
