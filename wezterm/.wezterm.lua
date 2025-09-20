7a8330a47dfb7776c6636a59905de6789b9284a8
chris <chris.schoony@gmail.com>
2025-09-20 15:47:42 -0700

    fix wezterm and update nvim plugins

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require("wezterm")
 local font_size = require("font_size")
+local window_decorations = os.getenv("WEZTERM_WINDOW_DECORATIONS")
 
 local config = wezterm.config_builder()
 
 config.color_scheme = "catppuccin-mocha"
 config.font = wezterm.font("Mononoki Nerd Font Mono")
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
+config.window_decorations = window_decorations and window_decorations or "RESIZE"
 
 font_size.set(config)
 
