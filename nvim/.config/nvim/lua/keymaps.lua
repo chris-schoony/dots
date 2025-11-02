091024bed57673ac89f498e94d10e557f3bc708c
chris <chris.schoony@gmail.com>
2025-11-01 23:49:41 -0700

    update plugins

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -9,7 +9,3 @@ vim.api.nvim_set_keymap("i", "kj", "<Esc>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>sv", "<C-w>v", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>sh", "<C-w>s", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>sc", "<C-w>c", { noremap = true, silent = true })
--- copy file, relative path, and absolute path to mac system clipboard
-vim.api.nvim_set_keymap("n", "<Leader>cf", ":let @*=expand('%:t')<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>cd", ":let @*=expand('%')<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>cD", ":let @*=expand('%:p')<CR>", { noremap = true, silent = true })
